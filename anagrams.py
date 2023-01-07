3ba9898219c39ee4391fd8b28e59226a3c58ed60
Kay Kim <kaykim@Kays-MacBook-Pro.local>
2023-01-06 21:06:00 -0500

    input and error check user's difficulty level

--- anagrams.py
+++ anagrams.py
@@ -41,5 +41,68 @@ def menu():
     print("Enter 3 for hard (6 letters)")
     print("Enter 4 to QUIT ")
     print()
+    
+    #Error check user input of the level using try/except 
+    
+    while True:
+        try:
+            level = int(input("Enter difficulty level: "))
+
+
+            #The entered level needs to be between 1 and 4
+            
+            if level < 1 or level > 4:
+                print("error")
+
+
+            #If the level is 1, it will display a set of 8 random letters
+                
+            if level == 1:
+                print()
+                print("Your letters are: ")
+                #insert letters here!!!
+                easy_letters = "easy random letters"
+
+                print (easy_letters)
+                return (easy_letters)
+
+               
+            #If the level is 2, it will display a set of 7 random letters
+            
+            elif level == 2:
+                print()
+                print("Your letters are: ")
+                #insert letters here!!!
+                mid_letters = "medium random letters"
+                
+                print (mid_letters)
+                return mid_letters
+
+
+            #If the level is 3, it will display a set of 6 random letters
+            
+            elif level == 3:
+                print()
+                print("Your letters are: ")
+                #insert letters here!!!
+                hard_letters = "hard random letters"
+
+                print(hard_letters)
+                return hard_letters
+
+
+            #If the user wants to leave the game before starting
+            
+            if level == 4:
+                leave = ()
+
+                #Double check that the user did not accidentally select wrong input
+                #to do later!!!
+
+        except:
+            print()
+            print("Please enter a number between 1-4")
+            print()
+
 
 menu()
